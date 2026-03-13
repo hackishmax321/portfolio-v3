b6f72ad9ccbbc420ea5b0f643efc2010f50a7e5f
hackishmax321 <hackishmax321@gmail.com>
2026-03-13 16:54:55 +0530

    new projects 2

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -10,9 +10,10 @@ function Frameworks() {
         <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
         <Icon src="https://cdn.worldvectorlogo.com/logos/next-js.svg" />
         <Icon src="https://icon-icons.com/download-file?file=https%3A%2F%2Fimages.icon-icons.com%2F2415%2FPNG%2F512%2Fangularjs_plain_logo_icon_146647.png&id=146647&pack_or_individual=pack" />
-        <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/1745px-Android_robot.svg.png" />
+        {/* <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Android_robot.svg/1745px-Android_robot.svg.png" /> */}
+        <Icon src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" />
         <Icon src="https://images-cdn.openxcell.com/wp-content/uploads/2024/07/25082439/reactnative-inner.svg" />
-        <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flutter_logo.svg/1024px-Flutter_logo.svg.png" />
+        <Icon src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" />
         <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Tensorflow_logo.svg/960px-Tensorflow_logo.svg.png" />
         <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Three.js_Icon.svg/2048px-Three.js_Icon.svg.png" />
         <Icon src="https://vite.dev/logo.svg" />
@@ -21,7 +22,7 @@ function Frameworks() {
       {/* Backend */}
       <OrbitingLogos radius={100} reverse>
         <Icon src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" />
-        <Icon src="https://cdn.worldvectorlogo.com/logos/expressjs.svg" />
+        {/* <Icon src="https://cdn.worldvectorlogo.com/logos/expressjs.svg" /> */}
         <Icon src="https://images.icon-icons.com/2699/PNG/512/python_logo_icon_168886.png" />
         <Icon src="https://cdn.worldvectorlogo.com/logos/fastapi.svg" />
         <Icon src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/960px-PHP-logo.svg.png" />
